215edccb614c9d687b79489ee19169e9f8f06b29
takashi.hiratani <tak752e@gmail.com>
2021-10-16 09:44:45 +0000

    set task

--- database/migrations/2021_10_16_182745_add_cotent_to_tasks_table.php
+++ database/migrations/2021_10_16_182745_add_cotent_to_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTasksTable extends Migration
+class AddCotentToTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,8 @@ class CreateTasksTable extends Migration
      */
     public function up()
     {
-        Schema::create('tasks', function (Blueprint $table) {
-            $table->bigIncrements('id');
+        Schema::table('tasks', function (Blueprint $table) {
             $table->string('content');
-            $table->timestamps();
         });
     }
 
@@ -27,6 +25,8 @@ class CreateTasksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tasks');
+        Schema::table('tasks', function (Blueprint $table) {
+            //
+        });
     }
 }
