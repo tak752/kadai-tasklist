7e76467230923b11253ea94c73b01b1cdbc1a3e4
takashi.hiratani <tak752e@gmail.com>
2021-10-16 14:13:24 +0000

    add destroy update

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -117,6 +117,12 @@ class TasksController extends Controller
      */
     public function destroy($id)
     {
-        //
+        // idの値でメッセージを検索して取得
+        $task = Task::findOrFail($id);
+        // メッセージを削除
+        $task->delete();
+
+        // トップページへリダイレクトさせる
+        return redirect('/');
     }
 }
