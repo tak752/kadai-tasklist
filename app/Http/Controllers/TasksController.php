0cb74bc5e63353f2d932ed2a8adc6d54212e95dc
takashi.hiratani <tak752e@gmail.com>
2021-10-17 22:45:50 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -49,12 +49,14 @@ class TasksController extends Controller
     {
         // バリデーション
         $request->validate([
-            'content' => 'required|max:255',
+            'content' => 'required',
+            'status' => 'required|max:10',
         ]);
         
         // タスクを作成
         $task = new Task;
         $task->content = $request->content;
+        $task->status = $request->status;       
         $task->save();
 
         // トップページへリダイレクトさせる
@@ -104,10 +106,17 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        // バリデーション
+        $request->validate([
+            'content' => 'required',
+            'status' => 'required|max:10',
+        ]);
+        
         // idの値でタスクを検索して取得
         $task = Task::findOrFail($id);
         // タスクを更新
         $task->content = $request->content;
+        $task->status = $request->status;    
         $task->save();
 
         // トップページへリダイレクトさせる
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -11,6 +11,8 @@
                 <div class="form-group">
                     {!! Form::label('content', 'タスク:') !!}
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
                 </div>
 
                 {!! Form::submit('更新', ['class' => 'btn btn-primary']) !!}
